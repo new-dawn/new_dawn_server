6fdb032bfdd4932f4fd2bc487bceaaa871a7eac9
l951010 <ljliniphone@gmail.com>
2018-11-11 22:00:47 -0500

    Add User Story Data Fields

--- new_dawn_server/users/models.py
+++ new_dawn_server/users/models.py
@@ -1,12 +1,18 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
+from phonenumber_field .modelfields import PhoneNumberField
+
 
 # An account model
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthday = models.DateField()
     creation_date = models.DateField(auto_now_add=True)
+    # a google's library for international phone number
+    phone_number = PhoneNumberField(default='000000000')
+    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')), default='M')
+
 
 # An account's profile information
 class Profile(models.Model):
@@ -17,3 +23,12 @@ class Profile(models.Model):
     )
     profile_photo_url = models.CharField(max_length=1000)
     description = models.CharField(max_length=200)
+    #city and hometown can later be changed to location library
+    city = models.CharField(max_length=50, default='new york')
+    employer = models.CharField(max_length=50, default='google')
+    job_title = models.CharField(max_length=50, default='analyst')
+    #School can be expanded further
+    school = models.CharField(max_length=50, default='nyu')
+    height = models.DecimalField(max_digits=2, decimal_places=2, default='1.12')
+    smoke = models.BooleanField(default=True)
+
