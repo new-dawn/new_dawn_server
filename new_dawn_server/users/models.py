8cf472e1c2d63a1618c957208bbd38142f12b2ad
l951010 <ljliniphone@gmail.com>
2018-11-11 23:44:25 -0500

    added hometown, changed fields order, remove google comments

--- new_dawn_server/users/models.py
+++ new_dawn_server/users/models.py
@@ -6,29 +6,28 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 # An account model
 class Account(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthday = models.DateField()
     creation_date = models.DateField(auto_now_add=True)
-    # a google's library for international phone number
-    phone_number = PhoneNumberField(default='000000000')
-    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')), default='M')
+    gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')), blank=True)
+    phone_number = PhoneNumberField(blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 # An account's profile information
 class Profile(models.Model):
     account = models.ForeignKey(
         User,
-        on_delete = models.CASCADE,
-        verbose_name = 'the user'
+        on_delete=models.CASCADE,
+        verbose_name='the user'
     )
-    profile_photo_url = models.CharField(max_length=1000)
+    # city and hometown can later be changed to location library
+    city_preference = models.CharField(max_length=50, blank=True)
     description = models.CharField(max_length=200)
-    #city and hometown can later be changed to location library
-    city = models.CharField(max_length=50, default='new york')
-    employer = models.CharField(max_length=50, default='google')
-    job_title = models.CharField(max_length=50, default='analyst')
-    #School can be expanded further
-    school = models.CharField(max_length=50, default='nyu')
-    height = models.DecimalField(max_digits=2, decimal_places=2, default='1.12')
-    smoke = models.BooleanField(default=True)
-
+    employer = models.CharField(max_length=50, blank=True)
+    height = models.DecimalField(max_digits=2, decimal_places=2, blank=True)
+    hometown = models.CharField(max_length=50, blank=True)
+    job_title = models.CharField(max_length=50, blank=True)
+    profile_photo_url = models.CharField(max_length=1000)
+    # school can be expanded further
+    school = models.CharField(max_length=50, blank=True)
+    smoke = models.BooleanField(blank=True)
