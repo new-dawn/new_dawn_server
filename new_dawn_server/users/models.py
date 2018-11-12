2a69c60cafc494a25e26a5103fe2c3bb571ec342
l951010 <ljliniphone@gmail.com>
2018-11-11 23:20:47 -0500

    remove space in importing phone number libraries

--- new_dawn_server/users/models.py
+++ new_dawn_server/users/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
-from phonenumber_field .modelfields import PhoneNumberField
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # An account model
