fa3eb46f734751a369af0448f6f9a80e65c2d574
duckmole <40241750+duckmoll@users.noreply.github.com>
2018-11-12 22:54:23 -0500

    Fixed bugs from last data model merge (#15)

--- new_dawn_server/users/models.py
+++ new_dawn_server/users/models.py
@@ -24,10 +24,10 @@ class Profile(models.Model):
     city_preference = models.CharField(max_length=50, blank=True)
     description = models.CharField(max_length=200)
     employer = models.CharField(max_length=50, blank=True)
-    height = models.DecimalField(max_digits=2, decimal_places=2, blank=True)
+    height = models.DecimalField(max_digits=2, decimal_places=2, blank=True, null=True)
     hometown = models.CharField(max_length=50, blank=True)
     job_title = models.CharField(max_length=50, blank=True)
     profile_photo_url = models.CharField(max_length=1000)
     # school can be expanded further
     school = models.CharField(max_length=50, blank=True)
-    smoke = models.BooleanField(blank=True)
+    smoke = models.BooleanField(blank=True, null=True)
