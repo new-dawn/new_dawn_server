e01bb09b7cb4a0421cff46cf49386bb05f27710b
Charlespartina <tangziyi001@gmail.com>
2018-11-12 23:22:43 -0500

    fix tab

--- pre_tests.py
+++ pre_tests.py
@@ -2,7 +2,7 @@ import filecmp
 
 def db_consistency_check():
 	if not filecmp.cmp('db.sqlite3', 'db.sqlite3.copy'):
-        raise Exception('db.sqlite3 is not in sync with the latest migration. Make sure you have run manage.py makemigrations & manage.py migrate')
+		raise Exception('db.sqlite3 is not in sync with the latest migration. Make sure you have run manage.py makemigrations & manage.py migrate')
 
 def main():
     db_consistency_check()
