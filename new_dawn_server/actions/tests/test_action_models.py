48c97413ae310c7e8a330cd164e23a45c8fce907
l951010 <ljliniphone@gmail.com>
2018-11-19 21:58:10 -0500

    Reformat for actions test case

--- new_dawn_server/actions/tests/test_action_models.py
+++ new_dawn_server/actions/tests/test_action_models.py
@@ -4,7 +4,6 @@ from new_dawn_server.users.models import Account
 from new_dawn_server.actions.models import UserToUserActionMetadata
 
 
-
 class LikeTest(TestCase):
     def setUp(self):
         Account.objects.create(
@@ -25,4 +24,3 @@ class LikeTest(TestCase):
             user_account_from=Account.objects.get(name="testuser_m"),
             user_account_to=Account.objects.get(name="testuser_f")
         )
-
