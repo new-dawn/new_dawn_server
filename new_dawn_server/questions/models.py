0a2579ade2f69f3d23bbd08413f2164dfceb74c8
l951010 <ljliniphone@gmail.com>
2018-11-23 11:19:04 -0500

    Added update time to models

--- new_dawn_server/questions/models.py
+++ new_dawn_server/questions/models.py
@@ -6,8 +6,10 @@ class Questions(models.Model):
                                      null=True, blank=True)
     user_question = models.CharField(max_length=150, blank=True)
     sample_answer = models.CharField(max_length=150, blank=True, null=True)
+    update_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
 
 class Answers(models.Model):
     question = models.OneToOneField(Questions, on_delete=models.CASCADE)
     answer = models.CharField(max_length=150, blank=True)
+    update_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
