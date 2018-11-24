3e151493f65f38d65d2dcae9d9c7fb0623d7b6c2
l951010 <ljliniphone@gmail.com>
2018-11-24 10:04:41 -0500

    Changed Answer Questions Model

--- new_dawn_server/questions/models.py
+++ new_dawn_server/questions/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
 
+class Answers(models.Model):
+    answer = models.CharField(max_length=150, blank=True)
+
+
 class Questions(models.Model):
+    question = models.CharField(max_length=150, blank=True)
+    sample_answer = models.CharField(max_length=150, blank=True, null=True)
+
+
+class AnswerQuestions(models.Model):
+    answer = models.ForeignKey(Answers, on_delete=models.CASCADE, blank=True)
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE, blank=True)
+    update_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+
+
+class UsersAnswerQuestions(models.Model):
+    answer_question = models.ForeignKey(AnswerQuestions, on_delete=models.CASCADE)
+    question_order = models.IntegerField()
     user_profile = models.ForeignKey("users.Profile", on_delete=models.CASCADE,
                                      null=True, blank=True)
-    user_question = models.CharField(max_length=150, blank=True)
-    sample_answer = models.CharField(max_length=150, blank=True, null=True)
-    update_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-
-
-class Answers(models.Model):
-    question = models.OneToOneField(Questions, on_delete=models.CASCADE)
-    answer = models.CharField(max_length=150, blank=True)
-    update_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
