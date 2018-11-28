fb2ef905a4aed6c803df5dd62fb053482bf24182
l951010 <ljliniphone@gmail.com>
2018-11-28 00:13:23 -0500

    Added question models to admin

--- new_dawn_server/questions/admin.py
+++ new_dawn_server/questions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from new_dawn_server.questions.models import AnswerQuestion
+from new_dawn_server.questions.models import Question
 
-# Register your models here.
+admin.site.register(AnswerQuestion)
+admin.site.register(Question)
--- new_dawn_server/users/admin.py
+++ new_dawn_server/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from new_dawn_server.users.models import Account
 from new_dawn_server.users.models import Profile
+
 admin.site.register(Account)
 admin.site.register(Profile)
