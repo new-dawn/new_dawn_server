dfcdec9a419d1b42af209fc6577fdaa962eaee14
Ziyi Tang <tangziyi001@gmail.com>
2018-12-02 21:30:38 -0500

    Create a command to create super user in prod (#45)
    * Add Create Super User Command
    
    * Remove tab
    
    * test db permission
    
    * restore

---
+++ new_dawn_server/management/commands/create_super_user.py
@@ -0,0 +1,13 @@
+import os
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+	def handle(self, *args, **options):
+		if 'SUPER_USER_NAME' in os.environ and 'SUPER_USER_EMAIL' in os.environ and 'SUPER_USER_PASSWORD' in os.environ:
+			username = os.environ['SUPER_USER_NAME']
+			if not User.objects.filter(username=username).exists():
+				User.objects.create_superuser(
+					username, os.environ['SUPER_USER_EMAIL'], os.environ['SUPER_USER_PASSWORD'])
