c817b96142c77f712ac7b5cd7f5d0c26fc9eea8e
Ziyi Tang <tangziyi001@gmail.com>
2018-12-05 00:34:04 -0500

    Add username/password login API. Response with Access Token (#49)
    * login
    
    * reformat

--- new_dawn_server/users/api/resources.py
+++ new_dawn_server/users/api/resources.py
@@ -1,15 +1,25 @@
 from django import forms
+from django.conf.urls import url
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.db import IntegrityError
 from django.db import transaction
+from django.db.models import signals
 from new_dawn_server.users.models import Account
 from new_dawn_server.users.models import Profile
 from tastypie import fields
 from tastypie.authentication import Authentication
 from tastypie.authorization import Authorization
 from tastypie.exceptions import BadRequest
+from tastypie.http import HttpUnauthorized, HttpForbidden
+from tastypie.models import create_api_key
 from tastypie.resources import ModelResource
+from tastypie.utils import trailing_slash
 from tastypie.validation import Validation
+
+# Create an API key whenever a User is created
+# This API key will be stored in front-end and be used
+# to access private views
+signals.post_save.connect(create_api_key, sender=User)
 
 # Fields of each user-related model
 # The bool value means if the field is required 
@@ -46,12 +56,54 @@ ACCOUNT_NAME_DELIMITER = "_"
 
 class UserResource(ModelResource):
     class Meta:
-        allowed_methods = ["get"]
+        allowed_methods = ["get", "post"]
         authentication = Authentication()
         authorization = Authorization()
         excludes = ["is_staff", "password"]
         queryset = User.objects.all()
         resource_name = "user"
+
+    def prepend_urls(self):
+        # Override user/login and user/logout urls with login/logout views
+        return [
+            url(r"^user/login/$", self.wrap_view("login"), name="api_login"),
+            url(r"^user/logout/$", self.wrap_view("logout"), name="api_logout"),
+        ]
+
+    def login(self, request, **kwargs):
+        self.method_check(request, allowed=["post"])
+
+        data = self.deserialize(request, request.body, format=request.META.get("CONTENT_TYPE", "application/json"))
+
+        username = data.get("username", "")
+        password = data.get("password", "")
+
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return self.create_response(request, {
+                    "success": True,
+                    "token": user.api_key.key,
+                })
+            else:
+                return self.create_response(request, {
+                    "success": False,
+                    "reason": "disabled",
+                }, HttpForbidden )
+        else:
+            return self.create_response(request, {
+                "success": False,
+                "reason": "incorrect",
+                }, HttpUnauthorized )
+
+    def logout(self, request, **kwargs):
+        self.method_check(request, allowed=["get"])
+        if request.user and request.user.is_authenticated():
+            logout(request)
+            return self.create_response(request, { "success": True })
+        else:
+            return self.create_response(request, { "success": False }, HttpUnauthorized)
 
 
 class AccountResource(ModelResource):
@@ -133,4 +185,10 @@ class UserRegisterResource(ModelResource):
                 **self._get_model_fields_dict(bundle, PROFILE_FIELDS)
             )
             profile.save()
+        return bundle
+
+    def dehydrate(self, bundle):
+        # Add extra fields to the response
+        bundle.data["username"] = bundle.obj.username
+        bundle.data["token"] = bundle.obj.api_key.key
         return bundle
