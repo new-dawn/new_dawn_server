1172436fe90b32405b17b713c42b5fa3af74ce04
Ziyi Tang <tangziyi001@gmail.com>
2018-12-15 14:43:57 -0500

    Unify Non-model Client Response into same module (#68)
    * response module with typecheck
    
    * Update code

---
+++ new_dawn_server/modules/tests/test_client_response.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+from new_dawn_server.modules.client_response import ClientResponse
+
+class ClientResponseTest(TestCase):
+    def setUp(self):
+        super().setUp()
+    
+    def test_client_response(self):
+        client_response = ClientResponse(success=True, message="Good Request", extra="other info")
+        response_dict = client_response.get_response_as_dict()
+        self.assertEqual(response_dict["success"], True)
+        self.assertEqual(response_dict["message"], "Good Request")
+        self.assertEqual(response_dict["extra"], "other info")
