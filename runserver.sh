ac8a11b74d192105fa02ea5e88a984e0ca2b8619
Ziyi Tang <tangziyi001@gmail.com>
2019-01-21 16:58:55 -0500

    [server] Auto create test users before running server (#91)
    * Auto create test users
    
    * fix
    
    * Cleanup d

--- runserver.sh
+++ runserver.sh
@@ -5,16 +5,18 @@
 
 echo "Setup OS Environment: Please make sure that you are already in your virtualenv, and has ran 'pip3 install -r requirements.txt'"
 
-echo "--- Create testing user: {username: testadmin, password: admintest}"
+echo "-------- Create testing user: {username: testadmin, password: admintest}"
 export SUPER_USER_NAME="testadmin"
 export SUPER_USER_PASSWORD="admintest"
 export SUPER_USER_EMAIL="superemail"
 
-echo "--- DB Migration"
+echo "-------- DB Migration"
+rm db.sqlite3
 python3 manage.py migrate
 
-echo "--- Pre-run Commands"
+echo "-------- Pre-run Commands"
 python3 manage.py create_super_user
+python3 manage.py create_test_users
 
-echo "--- Run Server"
+echo "-------- Run Server"
 python3 manage.py runserver
