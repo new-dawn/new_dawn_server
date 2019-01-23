52db928528a5e41586b78961ac0ed2b9c84fd7f1
l951010 <ljliniphone@gmail.com>
2019-01-22 22:43:15 -0500

    add like tests

--- new_dawn_server/actions/tests/test_action_models.py
+++ new_dawn_server/actions/tests/test_action_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from new_dawn_server.users.models import Account
-from new_dawn_server.actions.models import UserToUserActionMetadata
+from new_dawn_server.actions.models import Like, UserToUserActionMetadata
 
 
 class LikeTest(TestCase):
@@ -20,7 +20,39 @@ class LikeTest(TestCase):
             name="testuser_f",
             user=User.objects.create(username='nn')
         )
-        UserToUserActionMetadata.objects.create(
+
+        test_meta = UserToUserActionMetadata.objects.create(
             user_account_from=Account.objects.get(name="testuser_m"),
-            user_account_to=Account.objects.get(name="testuser_f")
+            user_account_to=Account.objects.get(name="testuser_f"),
+            entity_type="Picture",
+            entity_id=1
         )
+        Like.objects.create(like_metadata=test_meta)
+
+    def test_get_user_likes(self):
+        test_like_obj = Like.objects.get(like_metadata__user_account_from__name="testuser_m")
+        self.assertEqual(test_like_obj.like_metadata.user_account_to.name, "testuser_f")
+        self.assertEqual(test_like_obj.like_metadata.entity_id, 1)
+        self.assertEqual(test_like_obj.like_metadata.entity_type, "Picture")
+
+    def test_get_multiple_user_likes(self):
+        Account.objects.create(
+            birthday="1995-01-01",
+            gender="F",
+            phone_number="+100000000",
+            name="awa",
+            user=User.objects.create(username='ww')
+        )
+        Like.objects.create(like_metadata=UserToUserActionMetadata.objects.create(
+            user_account_from=Account.objects.get(name="testuser_m"),
+            user_account_to=Account.objects.get(name="awa"),
+            entity_type="Answer",
+            entity_id=1
+        ))
+        test_like_objs = Like.objects.filter(like_metadata__user_account_from__name="testuser_m")
+        self.assertEqual(test_like_objs[0].like_metadata.user_account_to.name, "testuser_f")
+        self.assertEqual(test_like_objs[0].like_metadata.entity_id, 1)
+        self.assertEqual(test_like_objs[0].like_metadata.entity_type, "Picture")
+        self.assertEqual(test_like_objs[1].like_metadata.user_account_to.name, "awa")
+        self.assertEqual(test_like_objs[1].like_metadata.entity_id, 1)
+        self.assertEqual(test_like_objs[1].like_metadata.entity_type, "Answer")
