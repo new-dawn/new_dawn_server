2c18d0f932e8661d1437847f4c5ec86c95742fd8
Charlespartina <tangziyi001@gmail.com>
2019-03-02 10:41:23 -0500

    two-side query

--- new_dawn_server/actions/api/resources.py
+++ new_dawn_server/actions/api/resources.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth.models import User
+from django.db.models import Q
 from new_dawn_server.actions.constants import ActionType, EntityType
 from new_dawn_server.actions.models import UserAction
 from new_dawn_server.modules.client_response import ClientResponse
@@ -37,6 +38,7 @@ class UserActionResource(ModelResource):
     def prepend_urls(self):
         return [
             url(r"^user_action/send_message/$", self.wrap_view("send_message"), name="api_send_message"),
+            url(r"user_action/get_messages/$", self.wrap_view("get_messages"), name="api_get_messages"),
         ]
 
     def hydrate_user_from(self, bundle):
@@ -72,5 +74,34 @@ class UserActionResource(ModelResource):
                     success=True,
                     message="Message Sent",
                 ).get_response_as_dict())
+
+    def build_message_response(self, messages):
+        result = []
+        for message_action in messages:
+            result.append({
+                "user_from": message_action.user_from,
+                "user_to": message_action.user_to,
+                "message": message_action.message
+            })
+        return result
+
+
+    def get_messages(self, request, **kwargs):
+        # Get messages action fetch all messages that
+        # 1. Sent by the requested user
+        # 2. Received by the user that the requested user chat with
+        # 3. Sorted by action creation time
+        self.method_check(request, allowed=["get"])
+        # TODO: Authenticate the user before allowing GET to go through
+        messages = UserAction.objects.filter(
+            (Q(user_from__id__exact=request.GET["user_from"]) & Q(user_to__id__exact=request.GET["user_to"]))
+            |
+            (Q(user_to__id__exact=request.GET["user_from"]) & Q(user_from__id__exact=request.GET["user_to"]))
+        ).order_by("update_time")
+        return self.create_response(request, ClientResponse(
+            success=True,
+            message="Message Get Successful",
+            objects=self.build_message_response(messages)
+        ).get_response_as_dict())
 
 
--- new_dawn_server/actions/api/tests/test_message.py
+++ new_dawn_server/actions/api/tests/test_message.py
@@ -11,7 +11,7 @@ from unittest.mock import patch
 class UserActionTest(ResourceTestCaseMixin, TestCase):
     def setUp(self):
         super().setUp()
-        self.user1_aruguments = {
+        self.user1_arguments = {
             "first_name": "test",
             "last_name": "user",
             "username": "duck",
@@ -70,38 +70,105 @@ class UserActionTest(ResourceTestCaseMixin, TestCase):
                     }
                 ]
         }
+        self.user3_arguments = {
+            "first_name": "test3",
+            "last_name": "user3",
+            "name": "goodgirl",
+            "username": "ziyi3",
+            "password": "test-pwd3",
+            "birthday": "1990-01-01",
+            "phone_number": "+12345678900",
+            "gender": "M",
+            "description": "nice",
+            "employer": "MANMAN",
+            "hometown": "NY",
+            "job_title": "CEO",
+            "profile_photo_url": "www",
+            "school": "NYU",
+            "smoke": "Socially",
+            "city_preference":
+                [
+                    {
+                        "city": "New York",
+                        "country": "US",
+                        "state": "NY"
+                    },
+                    {
+                        "city": "Salt Lake City",
+                        "country": "US",
+                        "state": "UT"
+                    }
+                ]
+        }
         self.api_client.post(
-            "/api/v1/register/", format="json", data=self.user1_aruguments)
+            "/api/v1/register/", format="json", data=self.user1_arguments)
         self.api_client.post(
             "/api/v1/register/", format="json", data=self.user2_arguments)
+        self.api_client.post(
+            "/api/v1/register/", format="json", data=self.user3_arguments)
 
         self.message_argument = {
             "user_from": "1",
             "user_to": "2",
             "message": "How are you"
         }
+        self.message_argument_2 = {
+            "user_from": "2",
+            "user_to": "1",
+            "message": "I'm good"
+        }
+        self.message_argument_3 = {
+            "user_from": "2",
+            "user_to": "3",
+            "message": "Nice to meet you"
+        }
 
     @patch("new_dawn_server.pusher.chat_service.ChatService.send")
     def test_message_user(self, send):
-        res = self.api_client.post(
+        res_1 = self.api_client.post(
             "/api/v1/user_action/send_message/", format="json", data=self.message_argument
         )
-        res_data = json.loads(res.content)
-        # Check post response
-        self.assertEqual(res_data["message"], "Message Sent")
-        self.assertEqual(res_data["success"], True)
+        res_2 = self.api_client.post(
+            "/api/v1/user_action/send_message/", format="json", data=self.message_argument_2
+        )
+        res_3 = self.api_client.post(
+            "/api/v1/user_action/send_message/", format="json", data=self.message_argument_3
+        )
+        res_all = [res_1, res_2, res_3]
+        for res in res_all:
+            res_data = json.loads(res.content)
+            # Check post response
+            self.assertEqual(res_data["message"], "Message Sent")
+            self.assertEqual(res_data["success"], True)
+
         # Check creation of objects
-        self.assertEqual(User.objects.count(), 2)
-        self.assertEqual(UserAction.objects.count(), 1)
-        test_like_object = UserAction.objects.get(user_from__id=1)
-        self.assertEqual(test_like_object.action_type, ActionType.MESSAGE.value)
-        self.assertEqual(test_like_object.entity_type, EntityType.NONE.value)
-        self.assertEqual(test_like_object.entity_id, 1)
-        self.assertEqual(test_like_object.user_to.username, "duck2")
-        self.assertEqual(test_like_object.message, "How are you")
-        # Test GET API for sender's message
+        self.assertEqual(User.objects.count(), 3)
+        self.assertEqual(UserAction.objects.count(), 3)
+        # Test all messages sent by user 1
+        test_message_object = UserAction.objects.get(user_from__id=1)
+        self.assertEqual(test_message_object.action_type, ActionType.MESSAGE.value)
+        self.assertEqual(test_message_object.entity_type, EntityType.NONE.value)
+        self.assertEqual(test_message_object.entity_id, 1)
+        self.assertEqual(test_message_object.user_to.username, "duck2")
+        self.assertEqual(test_message_object.message, "How are you")
+        # Test GET API for all messages between user 1 and user 2
         res = self.api_client.get(
-            "/api/v1/user_action/?user_from__id=1", format="json"
+            "/api/v1/user_action/get_messages/?user_from=1&user_to=2", format="json"
         )
         res_data = json.loads(res.content)
-        self.assertEqual(res_data["objects"][0]["message"], "How are you")
+        self.assertEqual(len(res_data["objects"]), 2)
+        res_data["objects"][0]["user_from"] = 1
+        res_data["objects"][0]["user_to"] = 2
+        res_data["objects"][0]["message"] = "How are you"
+        res_data["objects"][1]["user_from"] = 2
+        res_data["objects"][1]["user_to"] = 1
+        res_data["objects"][1]["message"] = "I'm good"
+
+        res = self.api_client.get(
+            "/api/v1/user_action/get_messages/?user_from=2&user_to=3", format="json"
+        )
+        res_data = json.loads(res.content)
+        self.assertEqual(len(res_data["objects"]), 1)
+        res_data["objects"][0]["user_from"] = 2
+        res_data["objects"][0]["user_to"] = 3
+        res_data["objects"][0]["message"] = "Nice to meet you"
