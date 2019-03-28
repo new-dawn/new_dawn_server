4965cfee83b44d043cb083b3cfaa2a15fe3463a2
Ziyi Tang <tangziyi001@gmail.com>
2019-03-27 22:23:18 -0700

    Create Test QA in server db (#121)
    * mess
    
    * fix
    
    * also return user id
    
    * nit
    
    * test qa

--- new_dawn_server/management/commands/create_test_actions.py
+++ new_dawn_server/management/commands/create_test_actions.py
@@ -8,7 +8,7 @@ from new_dawn_server.actions.models import UserAction
 class Command(BaseCommand):
 
 
-    def create_like(self, a, b):
+    def create_like_image(self, a, b):
         # Create Match
         print(f"Like from {a} to {b}")
         UserAction.objects.create(
@@ -19,6 +19,19 @@ class Command(BaseCommand):
             user_to = User.objects.get(id=b),
             message = "This looks great!"
         )
+
+    def create_like_answer(self, a, b):
+        # Create Match
+        print(f"Like from {a} to {b}")
+        UserAction.objects.create(
+            action_type = ActionType.LIKE.value,
+            entity_id = 1,
+            entity_type = EntityType.QUESTION_ANSWER.value,
+            user_from = User.objects.get(id=a),
+            user_to = User.objects.get(id=b),
+            message = "Sounds legit!"
+        )
+
     def create_match(self, a, b):
         # Create Match
         print(f"Relationship between {a} and {b}")
@@ -44,7 +57,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print("Create User Actions")
-        self.create_like(4,1)
+        self.create_like_image(4,1)
+        self.create_like_answer(3,1)
         self.create_match(1,2)
         self.create_match(1,3)
         self.create_message(1,2,"Hey there!")
