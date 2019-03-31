8ee605ea218163439d62aaef0b2f224ed485fd1a
l951010 <ljliniphone@gmail.com>
2019-03-31 16:07:01 -0400

    method to static

--- new_dawn_server/pusher/notification_service.py
+++ new_dawn_server/pusher/notification_service.py
@@ -7,15 +7,22 @@ from new_dawn_server.settings import (
 
 class NotificationService:
 
-    def __init__(self):
-        self.beams_client = PushNotifications(instance_id=BEAMS_INSTANCE_ID, secret_key=BEAMS_SECRET_KEY)
-
-    def beams_auth(self, user_id):
-        beams_token = self.beams_client.generate_token(str(user_id))
+    @staticmethod
+    def beams_auth(user_id):
+        beams_client = PushNotifications(
+            instance_id=BEAMS_INSTANCE_ID,
+            secret_key=BEAMS_SECRET_KEY,
+        )
+        beams_token = beams_client.generate_token(str(user_id))
         return beams_token
 
-    def send_notification(self, user_ids, message=None):
-        response = self.beams_client.publish_to_users(
+    @staticmethod
+    def send_notification(user_ids, message=None):
+        beams_client = PushNotifications(
+            instance_id=BEAMS_INSTANCE_ID,
+            secret_key=BEAMS_SECRET_KEY,
+        )
+        response = beams_client.publish_to_users(
             user_ids=user_ids,
             publish_body={
                 'apns': {
