192a1f2debd0f54f8fc4420c1fd5b220c71ccc11
duckmole <40241750+duckmoll@users.noreply.github.com>
2019-04-06 12:25:31 -0400

    Update with new images and question answers (#126)
    * delete images and questions when update
    
    * migrate database
    
    * delete link between images, answers and old profiles

--- new_dawn_server/users/api/tests/test_update_profile.py
+++ new_dawn_server/users/api/tests/test_update_profile.py
@@ -4,6 +4,8 @@ import json
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
 from django.test import TestCase
+from new_dawn_server.medias.models import Image
+from new_dawn_server.questions.models import AnswerQuestion
 from new_dawn_server.users.models import Account
 from new_dawn_server.users.models import Profile
 from tastypie.test import ResourceTestCaseMixin
@@ -33,6 +35,19 @@ class UserRegisterTest(ResourceTestCaseMixin, TestCase):
             "profile_photo_url": "www",
             "school": "NYU",
             "smoke": "Socially",
+            "answer_question":
+                [
+                    {
+                        "question": "how are you",
+                        "order": 1,
+                        "answer": "good",
+                    },
+                    {
+                        "question": "How do you do",
+                        "order": 2,
+                        "answer": "do do"
+                    }
+                ]
         }
 
         self.all_arguments = {
@@ -84,16 +99,46 @@ class UserRegisterTest(ResourceTestCaseMixin, TestCase):
                 ]
         }
 
+        # Post questions before answer
+        self.pre_defined_question_arguments = {
+            "question": "how are you",
+            "sample_answer": "ummm",
+            "user_defined": False
+        }
+        self.api_client.post("/api/v1/question/", format="json", data=self.pre_defined_question_arguments)
+        self.pre_defined_question_arguments = {
+            "question": "How do you do",
+            "sample_answer": "ummm",
+            "user_defined": False
+        }
+        self.api_client.post("/api/v1/question/", format="json", data=self.pre_defined_question_arguments)
+
+    def test_register_update_user(self):
         self.api_client.post(
             "/api/v1/register/", format="json", data=self.all_arguments)
-        # self.self_id = int(User.objects.first().id)
         self.api_client.post(
             "/api/v1/register/", format="json", data=self.user2_arguments)
 
-    def test_register_update_user(self):
+        self.test_data = {
+            "caption": "good",
+            "order": 1,
+            "user": "/api/v1/user/1/",
+        }
+        with open("media/images/test.png", "rb") as photo_file:
+            test_data_json = json.dumps(self.test_data)
+            full_data = {
+                "data": test_data_json,
+                "media": photo_file,
+            }
+            response = self.client.post(
+                '/api/v1/image/',
+                data=full_data,
+            )
+        self.assertEqual(Image.objects.count(), 1)
         self.assertEqual(User.objects.count(), 2)
         self.assertEqual(Account.objects.count(), 2)
         self.assertEqual(Profile.objects.count(), 2)
+        self.assertEqual(AnswerQuestion.objects.count(), 4)
         register_arguments_2 = {
             "first_name": "test-change",
             "username": "test-user",
@@ -129,6 +174,10 @@ class UserRegisterTest(ResourceTestCaseMixin, TestCase):
         self.assertEqual(User.objects.count(), 2)
         self.assertEqual(Account.objects.count(), 2)
         self.assertEqual(Profile.objects.count(), 2)
+        # Question Answers are kept after update
+        self.assertEqual(AnswerQuestion.objects.count(), 4)
+        # Images are kept after update
+        self.assertEqual(Image.objects.count(), 1)
         self.assertEqual(User.objects.get(id=1).first_name, "test-change")
         self.assertEqual(Account.objects.get(user_id=1).gender, "F")
         self.assertEqual(Profile.objects.get(user_id=1).description, "nice22222")
@@ -143,3 +192,25 @@ class UserRegisterTest(ResourceTestCaseMixin, TestCase):
         user_data_2 = res_data["objects"][0]
         self.assertEqual(user_data_2["user"]["first_name"], "test2")
         self.assertEqual(user_data_2["description"], "nice")
+
+        # Get profile should not get images from updated profile
+        self.assertEqual(len(user_data["images"]), 0)
+        self.assertEqual(len(user_data_2["images"]), 0)
+
+        # Updated image will link to updated profiles
+        with open("media/images/test.png", "rb") as photo_file:
+            test_data_json = json.dumps(self.test_data)
+            full_data = {
+                "data": test_data_json,
+                "media": photo_file,
+            }
+            response = self.client.post(
+                '/api/v1/image/',
+                data=full_data,
+            )
+        res = self.api_client.get("/api/v1/profile/", format="json")
+        res_data = json.loads(res.content)
+        user_data = res_data["objects"][1]
+        # You can only get one image from this profile after update
+        self.assertEqual(Image.objects.count(), 2)
+        self.assertEqual(len(user_data["images"]), 1)
