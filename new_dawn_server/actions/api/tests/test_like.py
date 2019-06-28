fa171fa38fbcf75c4bff66690382dca5496bcf6f
JaniceZhou <jz3031@nyu.edu>
2019-06-28 16:59:22 +0800

    recall taken action 4

--- new_dawn_server/actions/api/tests/test_like.py
+++ new_dawn_server/actions/api/tests/test_like.py
@@ -247,22 +247,7 @@ class UserActionTest(ResourceTestCaseMixin, TestCase):
             self.assertEqual(UserAction.objects.count(), 1)
             self.assertEqual(UserAction.objects.filter(action_type=ActionType.UNTAKEN.value).count(), 1)
 
-    def test_recall_taken(self):
-        with patch(
-                "new_dawn_server.pusher.notification_service.NotificationService._get_instance_id_and_secret_key",
-                return_value=["instance", "key"]
-        ), patch(
-            "new_dawn_server.pusher.notification_service.NotificationService.send_notification",
-            return_value=None
-        ), patch(
-            "new_dawn_server.pusher.notification_service.NotificationService.beams_auth",
-            return_value={
-                "token": "XXX"
-            }
-        ):
-            self.api_client.post(
-                "/api/v1/user_action/", format="json", data=self.taken_argument
-            )
+            # Recall taken
             recall_taken_argument = {
                 "action_type": ActionType.RECALL_TAKEN.value,
                 "user_from": "1",
@@ -271,7 +256,7 @@ class UserActionTest(ResourceTestCaseMixin, TestCase):
             self.api_client.post(
                 "/api/v1/user_action/", format="json", data=recall_taken_argument
             )
-            self.assertEqual(UserAction.objects.count(), 1)
+            self.assertEqual(UserAction.objects.count(), 2)
             self.assertEqual(UserAction.objects.filter(action_type=ActionType.RECALL_TAKEN.value).count(), 1)
 
     def test_match_user(self):
